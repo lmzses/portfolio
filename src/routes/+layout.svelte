285bc8d56ca7e9251051eba004b7fd9bf01d4ae1
lmzses <ml776566@gmail.com>
2024-07-04 09:24:41 -0500

    Add vercel analytics support.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script>
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
 	import Header from '../components/Header.svelte';
 	import '../app.css';
 	import Footer from '../components/Footer.svelte';
+
+	// Inject
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
