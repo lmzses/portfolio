03ffa02a1d63d1ff81ebebd0d1bc03148c3dfc9b
lmzses <ml776566@gmail.com>
2024-08-30 15:03:06 -0500

    Move main page to the (main) group layout.

---
+++ src/routes/(main)/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Main from '../../components/sections/Main.svelte';
+	import Projects from '../../components/sections/Projects.svelte';
+</script>
+
+<Main />
+<Projects />
--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import Main from '../components/sections/Main.svelte';
-	import Projects from '../components/sections/Projects.svelte';
-</script>
-
-<Main />
-<Projects />
